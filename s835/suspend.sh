969d44515961e6df6900aaea4b0d2cf9089a13f8
RogerF81 <lowhouse123@gmail.com>
2018-02-05 00:21:02 +0100

    Therefore lower suspend max freq on big bluster a bit

--- s835/suspend.sh
+++ s835/suspend.sh
@@ -607,7 +607,7 @@ else
 	chmod 664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 	echo 1670400 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 	echo $little_min_value > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-	echo 1651200 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+	echo 1420800 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
 	echo $big_min_value > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 	chmod 644 /sys/devices/system/cpu/online
 	echo "0-1,4-5" > /sys/devices/system/cpu/online
