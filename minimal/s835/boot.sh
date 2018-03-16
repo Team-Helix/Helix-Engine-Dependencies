601579613247e7c5a3361e94095c26c108b1e8fb
ZeroInfinity <team.helix.xda@gmail.com>
2018-03-16 09:31:36 +0000

    Fix LMK values

--- minimal/s835/boot.sh
+++ minimal/s835/boot.sh
@@ -3,19 +3,22 @@
 # Template by @ZeroInfinity, adapted from @RogerF81, improved by @Asiier
 # Helix-Engine profile script: Boot
 MEMTOTAL="$(< /proc/meminfo grep MemTotal | cut -d ":" -f2 | awk '{ print $1 }')"
-LMK='/sys/module/lowmemorykiller/parameters/minfree'
+LMK='/sys/module/lowmemorykiller/parameters'
 
-chmod 664 ${LMK}
-chmod 664 ${NET}/tcp_congestion_control
+chmod 644 ${LMK}/enable_adaptive_lmk
+chmod 664 ${LMK}/minfree
+
+echo '0' > ${LMK}/enable_adaptive_lmk
+echo '80' > /proc/sys/vm/overcommit_ratio
 
 if [ "${MEMTOTAL}" -gt "7361536" ] && [ "${MEMTOTAL}" -lt "8388608" ]; then
-	echo '18432,23040,27648,51256,150296,200640' > ${LMK}
+	echo '55296,82944,96768,144756,168750,243750' > ${LMK}/minfree
 elif [ "${MEMTOTAL}" -gt "5242880" ] && [ "${MEMTOTAL}" -lt "6291456" ]; then
-	echo '12288,15360,18432,21504,36864,53760' > ${LMK}
+	echo '41472,62208,72576,108567,126562,182812' > ${LMK}/minfree
 elif [ "${MEMTOTAL}" -gt "3145728" ] && [ "${MEMTOTAL}" -lt "4378624" ]; then
-	echo '18432,23040,27648,32256,55296,80640' > ${LMK}
+	echo '27648,41472,48384,72378,84375,121875' > ${LMK}/minfree
 elif [ "${MEMTOTAL}" -gt "2097152" ] && [ "${MEMTOTAL}" -lt "3145728" ]; then
-	echo '16662,20828,24993,29159,45600,67650' > ${LMK}
+	echo '19662,28828,24993,29159,45600,67650' > ${LMK}/minfree
 elif [ "${MEMTOTAL}" -gt "1048576" ] && [ "${MEMTOTAL}" -lt "2097152" ]; then
-	echo '14746,18432,22118,25805,40000,55000' > ${LMK}
+	echo '14746,12432,22118,25805,40000,55000' > ${LMK}/minfree
 fi
