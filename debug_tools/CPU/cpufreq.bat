06c80f2b638993f3acd4c33f5ddf583d626bd3b8
ZeroInfinity <team.helix.xda@gmail.com>
2018-03-28 13:13:07 +0100

    Change script to be more universal

--- debug_tools/CPU/cpufreq.bat
+++ debug_tools/CPU/cpufreq.bat
@@ -3,14 +3,18 @@ cls
 
 :loop
 echo "CPU0 MAX: "
-adb shell cat /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
+adb shell cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo "CPU0 MIN: "
-adb shell cat /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
+adb shell cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo "CPU0 GOV: "
+adb shell cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
 echo "CPU4 MAX: "
-adb shell cat /sys/devices/system/cpu/cpufreq/policy4/scaling_max_freq
+adb shell cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
 echo "CPU4 MIN: "
-adb shell cat /sys/devices/system/cpu/cpufreq/policy4/scaling_min_freq
+adb shell cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+echo "CPU4 GOV: "
+adb shell cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
 
 timeout /t 5 /nobreak
 goto loop
